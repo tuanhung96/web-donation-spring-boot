e5bf988786a4a56097000eda765bc149f901c6a7
tuanhung96 <tuanhung.hn96@gmail.com>
2023-07-30 18:38:57 +0700

    update 30/07

--- src/main/java/com/example/asm1/config/SecurityConfig.java
+++ src/main/java/com/example/asm1/config/SecurityConfig.java
@@ -51,6 +51,7 @@ public class SecurityConfig {
                                 .requestMatchers("/admin1/assets/**").permitAll()
                                 .requestMatchers("/user/assets/**").permitAll()
                                 .requestMatchers("/").permitAll()
+                                .requestMatchers("/home").permitAll()
                                 .requestMatchers("/register").permitAll()
                                 .requestMatchers("/admin/**").hasRole("ADMIN")
                                 .anyRequest().authenticated()
